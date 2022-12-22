5e0872a7eb0f4eb4ba4ebb8d9d6b25fbaf7b10f1
jkingslake <j.kingslake@columbia.edu>
2022-12-22 11:38:26 -0500

    add first 2 tests
    add first two tests: the file selection test and the loading test

---
+++ tests/test_file_selection_methods.py
@@ -0,0 +1,19 @@
+import pytest
+from xapres_package import ApRESDefs
+
+## Use two different methods from selecting the same ApRES DAT file from a google bucket. 
+#  In each case load it and then check that we have loaded the correct file. 
+
+def test_file_selection():
+    directory='gs://ldeo-glaciology/GL_apres_2022/A101'
+    xa1 = ApRESDefs.xapres(max_range=1400)
+    xa1.load_all(directory, 
+                remote_load = True,
+                file_numbers_to_process=[0,1])
+
+    xa2 = ApRESDefs.xapres(max_range=1400)
+    xa2.load_all(directory, 
+                remote_load = True,
+                file_names_to_process = xa1.dat_filenames_to_process)
+
+    assert xa1.data.identical(xa2.data)
