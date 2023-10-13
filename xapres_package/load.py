8f25013a0e61fdb72d6b14bef2f2ecf764eb0883
jkingslake <j.kingslake@columbia.edu>
2023-10-12 21:50:08 -0400

    move coherence and generate_range_diff to utils

--- xapres_package/load.py
+++ xapres_package/load.py
@@ -13,9 +13,7 @@ import xarray as xr
 import pandas as pd
 from tqdm import tqdm
 
-from .utils import phase2range, sonify, dB
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+from .utils import phase2range, sonify, dB, coherence, generate_range_diff
 
 def load_zarr(site = "A101", 
             directory = "gs://ldeo-glaciology/apres/greenland/2022/single_zarrs_noencode/"
@@ -641,110 +639,6 @@ class load_from_dat():
         self.logger.debug("debugging something")
         self.logger.info("some message")
         self.logger.error("something went wrong")
-    
-    def coherence(self, s1, s2):
-        """
-        Phase correlation between two elements of the scattering matrix
-        Jordan et al. (2019) eq. 13
-        Parameters
-        ---------
-        s1: array
-            first acquisition
-        s2:
-            second acquisition
-        Output
-        ---------
-        c:  array
-            phase coherence
-        """
-        top = np.einsum('ij,ij->i', s1, np.conj(s2))
-        bottom = np.sqrt(np.sum(np.abs(s1)**2,axis=1)*np.sum(np.abs(s2)**2,axis=1))
-        c = top/bottom
-
-        return c
-
-    def generate_range_diff(self, data1, data2, win_cor, step, range_ext=None, win_wrap=10, thresh=0.9, uncertainty='CR'):
-        """
-        Input data:
-        data1, data2: xarray.DataArrays describing the "profile" variable
-        range
-        """
-
-        # Get times of burst:
-        t1 = data1.time.data
-        t2 = data2.time.data
-        dt = (t2-t1)/ np.timedelta64(1, 's')
-        self.logger.info(f"Time between bursts : {dt}s")
-        # Get phase difference
-        
-        if range_ext is not None:
-            # Fill a depth array which will be more sparse than the full Range vector
-            idxs = np.arange(win_cor//2, range_ext.shape[0]-win_cor//2, step).astype(int)
-            ds = range_ext[idxs]
-        else:
-            idxs = np.arange(win_cor//2, data1.shape[1]-win_cor//2, step).astype(int)
-            ds = data1.profile_range[idxs]
-
-        # Create data and coherence vectors
-        acq1 = data1
-        acq2 = data2
-        co = np.empty_like(np.stack([ds.data]*data1.shape[0])).astype(np.cdouble)
-        for i, idx in enumerate(idxs):
-            # index two sub_arrays to compare
-            arr1 = acq1[:,idx-win_cor//2:idx+win_cor//2]
-            arr2 = acq2[:,idx-win_cor//2:idx+win_cor//2]
-            # correlation coefficient between acquisitions
-            # amplitude is coherence between acquisitions and phase is the offset
-            co[:,i] = self.coherence(arr1.data, arr2.data)
-        
-        # Phase unwrapping
-        phi = -np.angle(co).astype(float)
-        for i in range(co.shape[1]-1):
-            for t in range(co.shape[0]):
-                idx = i+1
-                if np.all(abs(co[t,idx-win_wrap:idx+win_wrap]) < thresh):
-                    continue
-                if phi[t,idx]-phi[t,idx-1] > np.pi:
-                    phi[t,idx:] -= 2.*np.pi
-                elif phi[t,idx]-phi[t,idx-1] < -np.pi:
-                    phi[t,idx:] += 2.*np.pi
-        # Range difference calculation
-        w = phase2range(phi,
-                         0.5608,
-                         ds.data,
-                         2e8,
-                         1.6823e8)
-
-        # If the individual acquisitions have had uncertainty calculations
-        
-        if uncertainty == 'CR':
-            # Error from Cramer-Rao bound, Jordan et al. (2020) Ann. Glac. eq. (5)
-            sigma = (1./abs(co))*np.sqrt((1.-abs(co)**2.)/(2.*win_cor))
-            # convert the phase offset to a distance vector
-            w_err = phase2range(sigma,
-                                    0.5608,
-                                    ds.data,
-                                    2e8,
-                                    1.6823e8)
-
-        '''elif uncertainty == 'noise_phasor':
-            # Uncertainty from Noise Phasor as in Kingslake et al. (2014)
-            # r_uncertainty should be calculated using the function phase_uncertainty defined in this script
-            r_uncertainty = phase2range(self, self.unc1, self.header.lambdac) +\
-                phase2range(self, self.unc2, self.header.lambdac)
-            idxs = np.arange(win//2, len(self.data)-win//2, step)
-            w_err = np.array([np.nanmean(r_uncertainty[i-win//2:i+win//2]) for i in idxs])'''
-        
-        coords = {'time':(['time'],t2,{'units': 'seconds','long_name':'Time of second burst'}),'profile_range':(['profile_range'],ds.profile_range.data,{'units': 'm','long_name':'Depth'})}
-        data_vars = {'time_diff':(['time'],np.cumsum(dt),{'units': 'seconds','long_name':'Time since first burst'}),
-                     'range_diff':(['time','profile_range'], w, #np.cumsum(w,axis=0), 
-                         {'units': 'm', 
-                          'long_name':'Range difference'}),
-            'err':(['time','profile_range'], w_err, 
-                         {'units': 'm', 
-                          'long_name':'Error'})}
-        ds_xr = xr.Dataset(data_vars=data_vars, coords=coords)
-        return ds_xr, co, phi # returning velocities in mm/day
  
 
 class DataFileObject:
