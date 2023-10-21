094916c2a30bbe95a04db63244f2a352950404b8
jkingslake <j.kingslake@columbia.edu>
2023-10-20 20:22:39 -0400

    add test of new wrapper fns

---
+++ tests/test_wrappers.py
@@ -0,0 +1,12 @@
+import pytest
+from xapres_package import load
+
+#  Test `generate_xarray` and `load_zarr` wrappers
+def test_wrappers():
+    
+    from_DAT_unattended = load.generate_xarray(directory='../../data/sample/single_dat_file/', 
+                file_numbers_to_process = [0], 
+                bursts_to_process=[0],
+                )
+
+    from_zarr = load.load_zarr()
